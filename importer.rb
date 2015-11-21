ebf5fb665d07dfc22e91ddf30f3a7ea20b1ff2a4
John Rees <john@bitsushi.com>
2015-11-21 18:10:29 +0000

    mysql working

--- importer.rb
+++ importer.rb
@@ -5,5 +5,21 @@ Bundler.require(:default)
 Figaro.application = Figaro::Application.new(environment: "production", path: "application.yml")
 Figaro.load
 
-puts ENV['test']
+class MySQL < ActiveRecord::Base
+  self.abstract_class = true
+  establish_connection(
+    :adapter  => 'mysql',
+    :database => ENV['mysql_database'],
+    :host     => ENV['mysql_host'],
+    :username => ENV['mysql_username'],
+    :password => ENV['mysql_password'],
+    :encoding => 'utf8',
+    :collation => 'utf8_general_ci'
+  )
+end
 
+class Device < MySQL 
+end
+
+puts Device.last.id
+ 
