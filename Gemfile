409773e4c98dd57a1ac42b2035679f98133106b9
John Rees <john@bitsushi.com>
2015-11-24 14:56:08 +0000

    updates

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'parallel'
-gem 'mysql2', platform: :ruby
+gem 'mysql', platform: :ruby
+gem 'activerecord', platform: :ruby
 gem 'jdbc-mysql', platform: :jruby
 gem 'activerecord-jdbc-adapter', platform: :jruby
 gem 'figaro'
--- importer.rb
+++ importer.rb
@@ -1,11 +1,11 @@
 require 'rubygems'
 require 'bundler'
-Bundler.require(:default)
-
+Bundler.require
+require 'active_record'
 Figaro.application = Figaro::Application.new(environment: "production", path: "application.yml")
 Figaro.load
 
-class MySQL < ActiveRecord::Base
+class My < ActiveRecord::Base
   self.abstract_class = true
   establish_connection(
     :adapter  => 'mysql',
@@ -15,12 +15,12 @@ class MySQL < ActiveRecord::Base
     :password => ENV['mysql_password'],
     :encoding => 'utf8',
     :collation => 'utf8_general_ci',
-    :pool => 16
+    :pool => 30
   )
 end
 
-class Feed < MySQL; end
-class Device < MySQL; end
+class Feed < My; end
+class Device < My; end
 
 class RawStorer
 
@@ -69,40 +69,46 @@ class RawStorer
   end
 
   def initialize device 
-
       keys = %w(temp bat co hum light nets no2 noise panel)
-
-      Feed.where(device_id: device.id).each do |feed|
-	begin
-      ts = feed.timestamp.to_i * 1000
-
-      _data = []
+      batch_size = 8000
+      count = Feed.where(device_id: device.id).count
+      count = count/batch_size
+      count += 1 if count%batch_size > 0
+      total = count
+      last_id = 0
       self.commands = []
-
-      keys.each do |sensor|
-        metric = sensor
-        value = feed[sensor]
-        value = method(sensor).call( (Float(value) rescue value), device.kit_version)
-        self.commands << "#{metric} #{ts} #{value} device=#{device.id} identifier=#{device.kit_version}"
+      while count >= 0
+        print "#{device.id} - #{count}/#{total}\n"
+        ids = Feed.where("device_id = ? and id > ?", device.id, last_id).limit(batch_size).ids
+        Feed.find(ids).each do |feed|
+	  begin
+            ts = feed.timestamp.to_i * 1000
+            keys.each do |sensor|
+              metric = sensor
+              value = feed[sensor]
+              value = method(sensor).call( (Float(value) rescue value), device.kit_version)
+              self.commands << "put #{metric} #{ts} #{value} device_id=#{device.id} identifier=sck#{device.kit_version}"
+            end
+          rescue Exception => e
+            raise "FAIL"
+          end
+        end
+        last_id = ids.last
+        count -= 1
       end
-
-    rescue Exception => e
-      commands = ['FAIL']
-    end
-end
   end
 
-
 end
 
-Parallel.each(Device.order(id: :desc).limit(500)) do |device|
-begin
-  File.open("imports/#{device.id}.txt", 'w') do |file|
-    file.write RawStorer.new(device).commands.join("\n")
-  end
-rescue
-puts device.id
-end
-end
+Parallel.each(Device.order(id: :desc)) do |device|
+		begin
+			File.open("imports/#{device.id}.txt", 'w') do |file|
+    				file.write RawStorer.new(device).commands.join("\n")
+  			end
+  			print [device.id,"OK\n".green].join("\t")
+		rescue Exception => e
+			print [device.id,"ERROR".red,"#{e.message.strip}\n"].join("\t")
+		end
+	end
 #Feed.connection.reconnect!
  
